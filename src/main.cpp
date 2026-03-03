91e8afb693ffb0ed860f71c623524d32e030ab7b
fami-gb <fami.gb0825@gmail.com>
2026-03-04 01:25:32 +0900

    feat: FastLEDを使った座標操作

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,47 @@
 #include <Arduino.h>
+#include <FastLED.h>
 
-const int BLACK_THRESHOLD = 1500;
-const int WHITE_THRESHOLD = 3000;
-const int SENSOR_PIN = 32;
-const int SEC = 1000;
+#define LED_PIN 2 // データ入力ピン
+#define NUM_LEDS 256 // マトリクス数
+#define BRIGHTNESS 20 // 明るさ
+#define LED_TYPE WS2812B
+#define COLOR_ORDER GRB
+
+// LEDの色データを保持する配列
+CRGB leds[NUM_LEDS];
+
+// (x, y)座標を、一次元配列のインデックスに変換する
+uint16_t XY(uint8_t x, uint8_t y) {
+    uint16_t index;
+
+    if (y & 0x01) {
+        // 奇数行は右から左へ
+        uint8_t reverseX = 15 - x;
+        index = (y * 16) + reverseX;
+    } else {
+        index = (y * 16) + x;
+    }
+    return index;
+}
 
 void setup() {
     Serial.begin(115200);
-    delay(SEC);
-    Serial.println("Hello, World!");
-    pinMode(SENSOR_PIN, INPUT);
+    delay(2000);
+
+    // FastLEDの初期化
+    FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+    FastLED.setBrightness(BRIGHTNESS);
+
+    Serial.println("LED Test Started");
 }
 
 void loop() {
-    int val = analogRead(SENSOR_PIN);
-    Serial.println(val);
+    FastLED.clear();
 
-    if (val < BLACK_THRESHOLD) {
-        Serial.println("BLACK");
-    } else if (val > WHITE_THRESHOLD) {
-        Serial.println("WHITE");
-    }
+    leds[XY(5, 5)] = CRGB::Red;
 
-    delay(2 * SEC);
+    leds[XY(10, 15)] = CRGB::Blue;
+
+    FastLED.show();
+    delay(1000);
 }
